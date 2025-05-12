c4eece353aef45d008baa2653390f1e0bba083e8
Bugra Ergin <bugra.ergin@hotelcard.com>
2025-05-12 18:16:43 +0200

    sitemap stuff 2

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\Tags\Url;
-use App\Models\Suggestion;
 
 class SitemapController extends Controller
 {
@@ -39,7 +38,7 @@ class SitemapController extends Controller
     {
         $sitemap = Sitemap::create();
 
-        // Add static pages
+        // Add core pages from your routes
         $sitemap->add(Url::create('/')
             ->setLastModificationDate(now())
             ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
@@ -47,34 +46,37 @@ class SitemapController extends Controller
 
         $sitemap->add(Url::create('/community-ideas')
             ->setLastModificationDate(now())
-            ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
             ->setPriority(0.9));
 
         $sitemap->add(Url::create('/login')
-            ->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY)
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
             ->setPriority(0.5));
 
         $sitemap->add(Url::create('/register')
-            ->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY)
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
             ->setPriority(0.5));
 
-        // Add API documentation pages
-        // Get the view data from getApiRoutes which contains API routes
-        $viewData = app(ServiceInfoController::class)->getApiRoutes();
+        // Add API docs index
+        $sitemap->add(Url::create('/docs')
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+            ->setPriority(0.8));
 
-        // Check if the view data contains API routes
-        if (isset($viewData->getData()['apis'])) {
-            $apis = $viewData->getData()['apis'];
+        // Add your API documentation routes
+        // You can hardcode a few key API routes here if you know them
+        $apiRoutes = [
+            // Example: Add your most important API documentation pages
+            // '/docs/auth',
+            // '/docs/api-keys',
+            // Add more as needed
+        ];
 
-            foreach ($apis as $api) {
-                if (isset($api['key'])) {
-                    $sitemap->add(
-                        Url::create("/docs/{$api['key']}")
-                            ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
-                            ->setPriority(0.8)
-                    );
-                }
-            }
+        foreach ($apiRoutes as $route) {
+            $sitemap->add(
+                Url::create($route)
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                    ->setPriority(0.7)
+            );
         }
 
         return $sitemap;
