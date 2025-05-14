1adb1d346b4b014a2eaf5f82a26db8cc112277ea
Bugra Ergin <bugra.ergin@hotelcard.com>
2025-05-14 11:28:47 +0200

    opcache preload

---
+++ bootstrap/cache/preload.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$map = require __DIR__ . '/../vendor/composer/autoload_classmap.php';
+foreach (array_keys($map) as $class) {
+    if (
+        ! class_exists($class, false)
+        && ! interface_exists($class, false)
+        && ! trait_exists($class, false)
+    ) {
+        @opcache_compile_file($map[$class]);
+    }
+}
+
+$files = [
+    __DIR__ . '/../app/Models',
+    __DIR__ . '/../app/Http/Controllers',
+    __DIR__ . '/../app/Services',
+];
+
+foreach ($files as $dir) {
+    foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir)) as $file) {
+        if ($file->getExtension() === 'php') {
+            @opcache_compile_file($file->getPathname());
+        }
+    }
+}
