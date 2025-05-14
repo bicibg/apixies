fdbe999b30e70e7235d0de62fac17d0ab1816111
Bugra Ergin <bugra.ergin@hotelcard.com>
2025-05-14 18:03:25 +0200

    debug

--- app/Http/Controllers/HtmlToPdfController.php
+++ app/Http/Controllers/HtmlToPdfController.php
@@ -12,20 +12,25 @@ class HtmlToPdfController extends Controller
         $request->validate(['html' => 'required|string']);
         $userHtml = $request->html;
 
-        // Render a full HTML document (head + body) via Blade
-        $fullHtml = view('docs.pdf.wrapper', ['content' => $userHtml])->render();
+        // Render a Blade wrapper that includes your CSS
+        $fullHtml = view('docs.pdf.wrapper', [
+            'content' => $userHtml,
+        ])->render();
 
-        $pdf = Browsershot::html($fullHtml)
+        $pdfContent = Browsershot::url(route('pdf.preview', ['html'=> base64_encode($fullHtml)]))
             ->noSandbox()
             ->waitUntilNetworkIdle()
             ->emulateMedia('screen')
             ->showBackground()
             ->pdf();
 
-        return response($pdf, 200, [
-            'Content-Type'        => 'application/pdf',
+
+        return response($pdfContent, 200, [
+            'Content-Type' => 'application/pdf',
             'Content-Disposition' => 'inline; filename="document.pdf"',
-            'X-Frame-Options'     => 'ALLOWALL',
+            'X-Frame-Options' => 'ALLOWALL',
+            'Cache-Control' => 'no-store, no-cache, must-revalidate, max-age=0',
         ]);
     }
+
 }
