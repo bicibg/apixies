4eeabfde405d45517d68a25d75d64e18fc56e71e
Bugra Ergin <bugra.ergin@hotelcard.com>
2025-05-14 11:41:31 +0200

    preload fix warnings

--- bootstrap/cache/preload.php
+++ bootstrap/cache/preload.php
@@ -2,13 +2,7 @@
 require __DIR__ . '/../../vendor/autoload.php';
 $map = require __DIR__ . '/../../vendor/composer/autoload_classmap.php';
 foreach (array_keys($map) as $class) {
-    if (
-        ! class_exists($class, false)
-        && ! interface_exists($class, false)
-        && ! trait_exists($class, false)
-    ) {
-        @opcache_compile_file($map[$class]);
-    }
+    @opcache_compile_file($map[$class]);
 }
 
 $files = [
@@ -18,14 +12,11 @@ $files = [
 ];
 
 foreach ($files as $dir) {
-    if (! is_dir($dir)) {
-        continue;
-    }
-    $iterator = new RecursiveIteratorIterator(
+    if (! is_dir($dir)) continue;
+    $it = new RecursiveIteratorIterator(
         new RecursiveDirectoryIterator($dir)
     );
-
-    foreach ($iterator as $file) {
+    foreach ($it as $file) {
         if ($file->getExtension() === 'php') {
             @opcache_compile_file($file->getPathname());
         }
