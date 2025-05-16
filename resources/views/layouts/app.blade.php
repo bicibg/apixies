2659ddf921e0a337e1525ca9350b6f0643a4a4a5
Bugra Ergin <bugra.ergin@hotelcard.com>
2025-05-16 18:27:23 +0200

    hardcode Apixies name for less headache

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description"
-          content="{{ config('app.name') }} - {{ config('app.tagline', 'Tiny APIs, Mighty Results') }}">
+          content="Apixies - {{ config('app.tagline', 'Tiny APIs, Mighty Results') }}">
 
     <title>@yield('title', 'Apixies')</title>
 
